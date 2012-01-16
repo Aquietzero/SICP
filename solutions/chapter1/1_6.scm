86fe7671d7275ce267b6ee69ef0b90a3de94d9ca
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-01-16 15:57:48 +0800

    add 1_6.scm

---
+++ solutions/chapter1/1_6.scm
@@ -0,0 +1,8 @@
+; Since scheme evaluate the expression in applicative-order,
+; every time the procedure `sqrt-iter` is called, `new-if`
+; is evaluated. And when `new-if` is evaluated, `sqrt-iter`
+; will be evaluated again. Such kind of cross procedure calls
+; between two procedures may lead to an infinite loop. Only
+; when procedure `good-enough?` is evaluated can the loop
+; terminate, but it just cannot happen if `new-if` is not a 
+; basic procedure.
---
+++ solutions/chapter1/square_root.scm
@@ -0,0 +1,23 @@
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x)
+               x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x) (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.00001))
+
+(define (mysqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
