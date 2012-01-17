f5dcfa4808acb6c8c4d89c1ea1e189e8e22ded7c
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-01-17 23:39:06 +0800

    add 1_10.scm

---
+++ solutions/chapter1/1_10.scm
@@ -0,0 +1,17 @@
+; Do not bother too much to consider the function from its
+; complicated form, just run it and find some patterns.
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+; (A 1 10) => 1024
+; (A 2 4)  => 65536
+; (A 3 3)  => 65536
+;
+; (define (f n) (A 0 n)) => f(n) = 2n
+; (define (g n) (A 1 n)) => g(n) = 2^n
+; (define (h n) (A 2 n)) => h(n) = 2^2...^2 (# of 2 is n)
