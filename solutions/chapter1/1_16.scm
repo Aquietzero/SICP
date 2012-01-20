d52c4ade80e57e1c2f468df7d2e5f99caca4eda9
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2012-01-20 17:28:09 +0800

    add 1_16.scm

---
+++ solutions/chapter1/1_16.scm
@@ -0,0 +1,7 @@
+(define (fast-expt b n)
+  (define (fast-expt-iter b n a)
+    (cond ((= n 0) a)
+          ((even? n) (fast-expt-iter (square b) (/ n 2) a))
+          (else (fast-expt-iter b (- n 1) (* a b)))))
+  (define (square x) (* x x))
+  (fast-expt-iter b n 1))
